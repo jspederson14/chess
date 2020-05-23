2416470c160e5b607495c5e6e2f9b098970b691e
jspederson14 <63327359+jspederson14@users.noreply.github.com>
2020-05-22 20:10:09 -0400

    Interface and classes
    some of the basic classes needed before GUI

---
+++ src/game/ChessGame.java
@@ -0,0 +1,23 @@
+package game;
+
+public class ChessGame {
+	
+	//checks piece at location
+	public String checkLocation(int x, int y) {
+		return "test";
+	}
+	
+	//checks win conditions
+	public boolean win() {
+		return false;
+	}
+	
+	//updates board
+	public void update() {
+		//update
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/pieces/Ipiece.java
@@ -0,0 +1,14 @@
+package pieces;
+
+public interface Ipiece {
+	void move (int x, int y);
+	//moves the piece
+	int checkX (Ipiece p);
+	//gets x coordinate
+	int checkY (Ipiece p);
+	//gets y coordinate
+	boolean isVaild(int x, int y);
+	//checks to see if a move is vaild
+	void capture();
+	//captures a piece and removes it from game
+}
